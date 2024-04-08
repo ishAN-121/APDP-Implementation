3e25a3afad68f6ad855514e47a7dc82c846088d4
Ishan <gargishan.1211@gmail.com>
2024-04-08 14:53:34 +0530

    full testing

--- code/ABAC.py
+++ code/ABAC.py
@@ -25,7 +25,7 @@ policy = Policy.from_json(policy_json)
 client = MongoClient()
 storage = MongoStorage(client)
 
-# storage.add(policy)
+#storage.add(policy)
 # storage.delete("5")
 pdp = PDP(storage)
 
@@ -44,59 +44,60 @@ t.calc()
 
 end = time.time()
 print("Precomputations took: ", end - start)
-number_of_iterations = [10,100,200,500]
+number_of_iterations = [1]
 total_time = 0
 errors = []
 rmse_values = [] 
 avg_time = []
 
-for j in number_of_iterations:
-    for i in range(j):
-        start = time.time()
-        with open('sample_request.json', 'r') as file:
-            request_json = json.load(file)
+# for j in number_of_iterations:
+#     for i in range(j):
+start = time.time()
+with open('sample_request.json', 'r') as file:
+    request_json = json.load(file)
 
-        user_attributes = []
+user_attributes = []
 
-        request = AccessRequest.from_json(request_json)
-        for k,v in request.subject.items():
-            user_attributes.append(v)
+request = AccessRequest.from_json(request_json)
+for k,v in request.subject.items():
+    user_attributes.append(v)
 
-        if pdp.is_allowed(request):
-            db = client['test-database']
-            test = db.test
-            C = t.comp_attr(user_attributes)
-            D = [1, 2, 3, 4, 5]
-            f_of_D = my_function(D)
-            senstivity = 1 #difference between two datasets
-            privacy_budget = np.exp(-1*C)
-            scale_parameter = senstivity/privacy_budget
-            f_of_D_with_noise = add_laplacian_noise_to_function(f_of_D, scale_parameter)
-            errors.append(f_of_D_with_noise - f_of_D)
-            end = time.time()
-            total_time += (end - start)
-        else:
-            print("Access denied")
-            exit(1)
+if pdp.is_allowed(request):
+    db = client['test-database']
+    test = db.test
+    C = t.comp_attr(user_attributes)
+    if request.action["method"] == "get":
+        document = test.find({"user_id":request.resource["name"]})
+        val = [d["value"] for d in document]
+    senstivity = 1 #difference between two datasets
+    privacy_budget = np.exp(-1*C)
+    scale_parameter = senstivity/privacy_budget
+    f_of_D_with_noise = add_laplacian_noise_to_function(val[0], scale_parameter)
+    errors.append(f_of_D_with_noise - val[0])
+    end = time.time()
+    total_time += (end - start)
+else:
+    print("Access denied")
+    exit(1)
 
-    sns.kdeplot(errors, fill=True)
-    plt.xlabel('Values')
-    plt.ylabel('Density')
-    plt.title('Kernel Density Estimation Plot')
-    plt.show()
+# sns.kdeplot(errors, fill=True)
+# plt.xlabel('Values')
+# plt.ylabel('Density')
+# plt.title('Kernel Density Estimation Plot')
+# plt.show()
 
-    print("Average time taken for each iteration: ", total_time/j)
-    print("RMSE: ", rmse(errors))
-    rmse_values.append(rmse(errors))
-    avg_time.append(total_time/j)
+print("Average time taken ", total_time)
+print("RMSE: ", rmse(errors))
+rmse_values.append(rmse(errors))
+avg_time.append(total_time)
 
-plt.bar(list(map(str,number_of_iterations)), rmse_values)
-plt.xlabel('Number of iterations')
-plt.ylabel('RMSE')
+# plt.bar(list(map(str,number_of_iterations)), rmse_values)
+# plt.xlabel('Number of iterations')
+# plt.ylabel('RMSE')
 
-plt.show()
+# plt.show()
 
-plt.plot(list(map(str,number_of_iterations)), avg_time)
-plt.xlabel('Number of iterations')
-plt.ylabel('Average time taken')
-plt.show()
+# plt.plot(list(map(str,number_of_iterations)), avg_time)
+# plt.xlabel('Number of iterations')
+# plt.ylabel('Average time taken')
+# plt.show()
